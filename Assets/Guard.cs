84537500e4c91d7afa1576a6aa095bd820e503a8
Rik <muijs@student.chalmers.se>
2021-06-28 13:05:31 +0200

    Initial commit

--- Assets/Guard.cs
+++ Assets/Guard.cs
@@ -2,9 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Guard : MonoBehaviour
-{
-    
+public class Guard : MonoBehaviour {
+
+    public Transform pathHolder;
+
+    private void OnDrawGizmos() {
+        foreach (Transform waypoint in pathHolder) {
+            Gizmos.DrawCube(waypoint.position, Vector3.one*.1f);
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
