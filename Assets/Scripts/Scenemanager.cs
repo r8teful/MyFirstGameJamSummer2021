d927bb70dfff70d8329544eed07456cd908091c0
rikmuijs <muijs@student.chalmers.se>
2021-07-01 20:15:25 +0200

    Added CameraGuards & 3rd level

--- Assets/Scripts/Scenemanager.cs
+++ Assets/Scripts/Scenemanager.cs
@@ -16,18 +16,6 @@ public class Scenemanager : MonoBehaviour {
         }
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Debug.Log("Current scene is" + SceneManager.GetActiveScene().buildIndex);
-    }
-
     public void LoadNextLevel() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
